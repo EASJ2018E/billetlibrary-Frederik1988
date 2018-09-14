46140f1f56477771f2726020f61731d77ac75857
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-09-14 12:32:30 +0200

    Klassen MC oprettet og testet. Samme problem med coverage som før hvor den autogenerede klasse ikke kan testet.

---
+++ BilletLibrary.tests/MCTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BilletLibrary.tests
+{
+    [TestClass]
+    public class MCTest
+    {
+        [TestMethod]
+        public void Pris()
+        {
+            //Arrange
+            var mc = new MC();
+
+            //Act
+            var pris = mc.Pris();
+
+            //Assert
+            Assert.AreEqual(125, pris);
+        }
+
+        [TestMethod]
+
+        public void Køretøj()
+        {
+            //Arrange
+            var mc = new MC();
+
+            //Act
+            string køretøj = "MC";
+
+            //Assert
+            Assert.AreSame(køretøj, mc.Køretøj());
+
+        }
+    }
+}
---
+++ BilletLibrary/MC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilletLibrary
+{
+    /// <summary>
+    /// En klasse for køretøjer som er motorcykler
+    /// </summary>
+    public class MC
+    {
+        public string NummerPlade { get; set; }
+
+        public DateTime Dato { get; set; }
+
+        public decimal Pris()
+        {
+            return 125;
+        }
+
+        public string Køretøj()
+        {
+            return "MC";
+        }
+    }
+}
